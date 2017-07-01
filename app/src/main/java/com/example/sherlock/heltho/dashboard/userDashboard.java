cd3d99f4af72710852d5b2aae781bf23094e37d8
getsanjeev <getsanjeevdubey@gmail.com>
2017-07-01 16:28:32 +0530

    added searchview action bar

--- app/src/main/java/com/example/sherlock/heltho/dashboard/userDashboard.java
+++ app/src/main/java/com/example/sherlock/heltho/dashboard/userDashboard.java
@@ -14,7 +14,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import com.example.sherlock.heltho.R;
 
 
@@ -38,7 +37,6 @@ public class userDashboard extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
