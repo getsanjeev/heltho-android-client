ded2e018c0dea3b6120e318a18b8b2b7878d4526
getsanjeev <getsanjeevdubey@gmail.com>
2017-07-01 23:44:54 +0530

    populating listview resolved one item fix

--- app/src/main/java/com/example/sherlock/heltho/dashboard/searchResultActivity.java
+++ app/src/main/java/com/example/sherlock/heltho/dashboard/searchResultActivity.java
@@ -2,9 +2,12 @@ package com.example.sherlock.heltho.dashboard;
 
 import android.app.SearchManager;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -45,8 +48,7 @@ public class searchResultActivity extends AppCompatActivity {
     }
 
     private void loadSuggestions(){
-
-
+        populate_list(count);
     }
 
     private void populate_list(int count){
@@ -54,9 +56,30 @@ public class searchResultActivity extends AppCompatActivity {
         String [] restaurant_names = new String[10];
         String [] money = new String[10];
         String [] delivery_details = new String[10];
+        Bitmap [] photo = new Bitmap[10];
+        String mdish_names[] = {"Tomato Bruchetta","New McDonalds Columbia Pack","Chilly Potato","Italian Fig Bruchetta","New Paneer Wrap",
+        "McDonalds Extra Value Pack","McDonalds 007 Hero Burger","Pork Sausage Pizza","Stanley FarmHouse Pizza","McDonalds McPuff"};
+        String [] mrestaurant_names = {"Hudson Caffe","Flamess Restaurant & Cafe Village","Shagun Restaurant","Nandhini Udupi Restaurant",
+        "Sagar Ratna","Rico's World Street Food ","QD's Restaurant","QD's Restaurant","Slice Of Italy","Aam Aadmi Pakwaan"};
+        int mmoney[] = {125,222,499,199,111,989,79,169,350,120};
+        String mdelivery_details[] = {"30min","25min","40min","10min","Just Now","25 min","10min","Just Now","25 min","55min"};
+        Bitmap mphoto[] = {BitmapFactory.decodeResource(getResources(), R.drawable.bruchetta),
+                BitmapFactory.decodeResource(getResources(), R.drawable.columbia),
+                BitmapFactory.decodeResource(getResources(), R.drawable.cp),
+                BitmapFactory.decodeResource(getResources(), R.drawable.figbruschetta),
+                BitmapFactory.decodeResource(getResources(), R.drawable.mcspicy_paneer_wrap),
+                BitmapFactory.decodeResource(getResources(), R.drawable.hero_extra_value),
+                BitmapFactory.decodeResource(getResources(), R.drawable.mcdonalds_big_mac_007_hero),
+                BitmapFactory.decodeResource(getResources(), R.drawable.ii),
+                BitmapFactory.decodeResource(getResources(), R.drawable.stanley_fh),
+                BitmapFactory.decodeResource(getResources(), R.drawable.pizza_mcpuff)};
 
-        lv.setAdapter(new customAdapter(this,dish_names,restaurant_names,money,delivery_details));
+        Log.e("dsd",Integer.toString(mmoney.length) + mdelivery_details.length+mdish_names.length+mrestaurant_names.length+mphoto.length);
+
+        lv.setAdapter(new customAdapter(this,mphoto,mdish_names,mrestaurant_names,mmoney,mdelivery_details));
     }
+
+
 
 
 
@@ -70,7 +93,7 @@ public class searchResultActivity extends AppCompatActivity {
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
             Toast.makeText(this, "HELLO "+query, Toast.LENGTH_SHORT).show();
-
+            loadSuggestions();
             //use the query to search your data somehow
         }
     }
--- app/src/main/java/com/example/sherlock/heltho/utilities/customAdapter.java
+++ app/src/main/java/com/example/sherlock/heltho/utilities/customAdapter.java
@@ -1,10 +1,13 @@
 package com.example.sherlock.heltho.utilities;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.sherlock.heltho.R;
@@ -17,22 +20,26 @@ public class customAdapter  extends BaseAdapter {
     private Context context;
     private final String[] dishName;
     private final String[] restaurantName;
-    private final String[] money_info;
+    private final int[] money_info;
     private final String[] deliveryTime;
+    private final Bitmap[] photo;
 
-    public customAdapter(Context context, String[] transfer_details,
-                         String [] restaurantName,String[]money, String[] transactionID) {
+    public customAdapter(Context context, Bitmap[] photo, String[] transfer_details,
+                         String [] restaurantName, int[]money, String[] transactionID) {
         this.context = context;
         this.dishName = transfer_details;
         this.restaurantName = restaurantName;
         this.money_info = money;
         this.deliveryTime = transactionID;
+        this.photo = photo;
+
     }
 
     TextView dish_name;
     TextView restaurant_name;
     TextView delivery_time;
     TextView money;
+    ImageView food_pic;
 
     @Override
     public int getCount() {
@@ -40,17 +47,20 @@ public class customAdapter  extends BaseAdapter {
     }
 
     @Override
-    public View getView(final int position, final View convertView, ViewGroup parent) {
+    public View getView(int position, final View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        Log.e("position",Integer.toString(position));
         View rowView = inflater.inflate(R.layout.my_food_list_view_item, parent, false);
         dish_name = (TextView)rowView.findViewById(R.id.dish_name);
         restaurant_name = (TextView)rowView.findViewById(R.id.restaurant_name);
         delivery_time  =(TextView)rowView.findViewById(R.id.delivery_time);
         money = (TextView)rowView.findViewById(R.id.money_transacted);
+        food_pic = (ImageView)rowView.findViewById(R.id.pizza_symbol);
         dish_name.setText(dishName[position]);
         restaurant_name.setText("Order from: "+restaurantName[position]);
         delivery_time.setText("Expected Delivery: "+deliveryTime[position]);
-        money.setText(money_info[position]);
+        money.setText(Integer.toString(money_info[position]));
+        food_pic.setImageBitmap(photo[position]);
         return rowView;
     }
 
