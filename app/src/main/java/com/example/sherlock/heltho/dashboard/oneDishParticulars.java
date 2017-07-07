861e3cca2c19878c75e7561264ec31c9ab321191
getsanjeev <getsanjeevdubey@gmail.com>
2017-07-07 14:20:26 +0530

    added user ratings UI & MPchart

--- app/src/main/java/com/example/sherlock/heltho/dashboard/oneDishParticulars.java
+++ app/src/main/java/com/example/sherlock/heltho/dashboard/oneDishParticulars.java
@@ -5,20 +5,36 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import com.example.sherlock.heltho.R;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.utils.ViewPortHandler;
 import com.r0adkll.slidr.Slidr;
 import com.r0adkll.slidr.model.SlidrConfig;
 import com.r0adkll.slidr.model.SlidrListener;
 import com.r0adkll.slidr.model.SlidrPosition;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by sherlock on 7/7/17.
  */
 
 public class oneDishParticulars extends AppCompatActivity {
+
+    BarChart chart;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.one_item_particulars);
+        chart = (BarChart)findViewById(R.id.graph_ratings);
         Slidr.attach(this);
         SlidrConfig config = new SlidrConfig.Builder()
                 .primaryColor(getResources().getColor(R.color.colorPrimary))
@@ -56,5 +72,48 @@ public class oneDishParticulars extends AppCompatActivity {
                                 .build();
 
         Slidr.attach(this, config);
+        setRatingChart();
     }
+
+    void setRatingChart(){
+
+//        chart.getXAxis().setCenterAxisLabels(false);
+//        chart.getXAxis().setDrawGridLines(false);
+//        chart.getXAxis().setDrawLabels(false);
+        chart.getLegend().setEnabled(false);
+        chart.getXAxis().setEnabled(false);
+        chart.getAxisLeft().setEnabled(false);
+        chart.getAxisRight().setEnabled(false);
+        chart.setTouchEnabled(false);
+        chart.setDrawingCacheBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        Description ds = new Description();
+        ds.setText("");
+        chart.setDescription(ds);
+
+
+
+        List<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0f, 30f, "5★"));
+        entries.add(new BarEntry(1f, 80f, "4★"));
+        entries.add(new BarEntry(2f, 60f, "3★"));
+        entries.add(new BarEntry(3f, 50f, "2★"));
+        entries.add(new BarEntry(4f, 70f, "1★"));
+
+        BarDataSet set = new BarDataSet(entries,"User Ratings");
+        BarData data = new BarData(set);
+        data.setBarWidth(0.5f);
+        data.setValueFormatter(new IValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+                return entry.getData().toString();
+            }
+        });
+
+
+        chart.setData(data);
+        chart.setFitBars(true); // make the x-axis fit exactly all bars
+        chart.invalidate(); // refresh
+    }
+
 }
+
